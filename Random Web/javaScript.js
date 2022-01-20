84fe014f985eb908b18ebfeb19e19e546809a2c0
Ruler of the Soviet Union <74605307+creppzguy@users.noreply.github.com>
2022-01-20 05:56:56 +0100

    Add files via upload

--- Random Web/javaScript.js
+++ Random Web/javaScript.js
@@ -6,6 +6,11 @@ randomlinks[3]="https://www.youtube.com/watch?v=9TjXanLjpTU";
 randomlinks[4]="https://www.youtube.com/watch?v=7hZS3N_FWPI";
 randomlinks[5]="https://www.youtube.com/watch?v=Lq9eqHDKJPE";
 randomlinks[6]="https://www.youtube.com/watch?v=dQw4w9WgXcQ0";
+randomlinks[7]="https://www.youtube.com/watch?v=3cqV5BKJHyk";
+randomlinks[8]="https://www.youtube.com/watch?v=goXDAFtkJLw";
+randomlinks[9]="https://www.youtube.com/watch?v=PTn23s0BjEo";
+randomlinks[10]="https://www.youtube.com/watch?v=RunM5a7otLA";
+randomlinks[11]="https://www.youtube.com/watch?v=-GqahEAOhfU";
 
 
 function randomlink(){
