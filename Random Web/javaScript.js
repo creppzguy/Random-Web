9f5f6e5ac0f7b94458cc9d8bd7e91a27174e84f4
Ruler of the Soviet Union <74605307+creppzguy@users.noreply.github.com>
2022-01-26 04:42:13 +0100

    Add files via upload
    Elég sok bugfixel voltak itt tesó

--- Random Web/javaScript.js
+++ Random Web/javaScript.js
@@ -11,8 +11,51 @@ randomlinks[8]="https://www.youtube.com/watch?v=goXDAFtkJLw";
 randomlinks[9]="https://www.youtube.com/watch?v=PTn23s0BjEo";
 randomlinks[10]="https://www.youtube.com/watch?v=RunM5a7otLA";
 randomlinks[11]="https://www.youtube.com/watch?v=-GqahEAOhfU";
+randomlinks[12]="https://www.youtube.com/watch?v=ZmJ5oBdJTXQ";
+randomlinks[13]="https://www.youtube.com/watch?v=4jQPwxP83Oo";
+randomlinks[14]="https://www.youtube.com/watch?v=WBs3G1PvyfM";
+randomlinks[15]="https://www.youtube.com/watch?v=UjERYchpESk";
+randomlinks[16]="https://www.youtube.com/watch?v=U06jlgpMtQs";
+randomlinks[17]="https://www.youtube.com/watch?v=62EH5quV6dM";
+randomlinks[18]="https://www.youtube.com/watch?v=4jQPwxP83Oo";
+randomlinks[19]="https://www.youtube.com/watch?v=Jr3Z9qtBk7k";
+randomlinks[20]="https://www.youtube.com/watch?v=GxWiRU1wrPo";
+randomlinks[21]="https://www.youtube.com/watch?v=rXr-Kfa4_cY";
+randomlinks[22]="https://www.youtube.com/watch?v=mobtxEJHhY4";
+randomlinks[23]="https://www.youtube.com/watch?v=D1AYngyOrZs";
+randomlinks[24]="https://www.youtube.com/watch?v=7JStbvjy8k0";
+randomlinks[25]="https://www.youtube.com/watch?v=QXYOqguWx04";
+randomlinks[26]="https://www.youtube.com/watch?v=4qskDwvwmGI";
+randomlinks[27]="https://www.youtube.com/watch?v=aZ7htQofqnE";
 
 
 function randomlink(){
   window.open(randomlinks[Math.floor(Math.random()*randomlinks.length)]);
 }
+
+var Magyarországs = [];
+Magyarországs[0]="https://www.youtube.com/watch?v=o-JMc7Ug9vs&t=26564s";
+Magyarországs[1]="https://www.youtube.com/watch?v=Tdf7wbCbfkM";
+Magyarországs[2]="https://www.youtube.com/watch?v=3jJbhqV1Izc&t=50s";
+Magyarországs[3]="https://www.youtube.com/watch?v=rXPffsvIinQ&t=1s";
+Magyarországs[4]="https://www.youtube.com/watch?v=fVtaY_MGyQY&t=21580s";
+Magyarországs[5]="https://www.youtube.com/watch?v=K_03kFqWfqs";
+Magyarországs[6]="https://www.youtube.com/watch?v=E3Huy2cdih0&t=80s";
+Magyarországs[7]="https://www.youtube.com/watch?v=WdooJUb58Gc";
+Magyarországs[8]="https://www.youtube.com/watch?v=42tV6oM48NQ&t=6544s";
+Magyarországs[9]="https://www.youtube.com/watch?v=0L0y9KrteMU";
+Magyarországs[10]="https://www.youtube.com/watch?v=eN4dDc-EvFY";
+Magyarországs[11]="https://www.youtube.com/watch?v=F-ZDZKuEv9o";
+Magyarországs[12]="https://www.youtube.com/watch?v=ngx-FW-zqAw";
+Magyarországs[13]="https://www.youtube.com/watch?v=KQoHgDtDRII";
+Magyarországs[14]="https://www.youtube.com/watch?v=E2PioW-zjpY&t=2211s";
+Magyarországs[15]="https://www.youtube.com/watch?v=N6C88nC9s1U";
+Magyarországs[16]="https://www.youtube.com/watch?v=8XX3uAvf0PI";
+Magyarországs[17]="https://www.youtube.com/watch?v=OiMJz1OaVSA";
+Magyarországs[18]="https://www.youtube.com/watch?v=YRLz1QnuTdg";
+Magyarországs[19]="https://www.youtube.com/watch?v=1BvGbHomdps";
+Magyarországs[20]="https://www.youtube.com/watch?v=hVKnaTWZfMs";
+
+function Magyarország(){
+    window.open(Magyarországs[Math.floor(Math.random()*Magyarországs.length)]);
+  }
